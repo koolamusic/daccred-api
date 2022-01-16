2ecd049de6b99f9de120a40d88f8aeb0bb9739a7
Andrew Miracle <me@andrewmiracle.com>
2022-01-16 17:39:38 +0000

    fix linting issued

--- src/lib/ipfs/crypto.js
+++ src/lib/ipfs/crypto.js
@@ -1,2 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-console */
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 const {randomUUID} = require('crypto')
 console.log(randomUUID())
